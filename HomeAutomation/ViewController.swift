eb71cdcedd45110a88b3466e67a43b6f9abc80ee
Lakshank Patel <lpat0007@student.monash.edu>
2019-10-11 13:26:03 +1100

    Initial Commit

---
+++ HomeAutomation/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HomeAutomation
+//
+//  Created by Lakshank Patel on 11/10/19.
+//  Copyright © 2019 Monash University. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
